d6851a2f99983ca9bbbfef8ee73013829e9db105
Thomas Guérin <guerin.thomas35000@gmail.com>
2018-12-16 19:50:58 +0100

    Decouvrage des voisins wifi P2P et UI du mode multijoueur

--- app/src/main/java/com/iot/jeupromob/activity/MainActivity.java
+++ app/src/main/java/com/iot/jeupromob/activity/MainActivity.java
@@ -10,17 +10,6 @@ import com.iot.jeupromob.R;
 import com.iot.jeupromob.util.WifiP2PBroadcastReceiver;
 
 public class MainActivity extends AppCompatActivity {
-    //IntentFilter pour les events P2P
-    private final IntentFilter mIntentFilter = new IntentFilter();
-    private WifiP2pManager.Channel mChannel;
-    private WifiP2pManager mWifiP2PManager;
-    private WifiP2PBroadcastReceiver mWifiP2PReceiver;
-
-    private boolean mIsWifiP2PEnabled = false;
-    public void setIsWifiP2pEnabled(boolean value){
-        mIsWifiP2PEnabled = value;
-        //TO DO : Prevenir l'user sur l'UI si le wiki P2P est désactivé
-    }
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -28,43 +17,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, new GameMenuFragment()).commit();
-
-        //Vérifie si le Wifi P2P est bien activé
-        if(WifiP2pManager.EXTRA_WIFI_STATE.equals(WifiP2pManager.WIFI_P2P_STATE_ENABLED)){
-            mIsWifiP2PEnabled = true;
-            // Indicates a change in the Wi-Fi P2P status.
-            mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-            // Indicates a change in the list of available peers.
-            mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-            // Indicates the state of Wi-Fi P2P connectivity has changed.
-            mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-            // Indicates this device's details have changed.
-            mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-            mWifiP2PManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-            mChannel = mWifiP2PManager.initialize(this, getMainLooper(), null);
-            mWifiP2PReceiver = new WifiP2PBroadcastReceiver(mWifiP2PManager, mChannel, this);
-        }
-
-
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-    }
-
-    /** register the BroadcastReceiver with the intent values to be matched */
-    @Override
-    public void onResume() {
-        super.onResume();
-        mWifiP2PReceiver = new WifiP2PBroadcastReceiver(mWifiP2PManager, mChannel, this);
-        registerReceiver(mWifiP2PReceiver, mIntentFilter);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        unregisterReceiver(mWifiP2PReceiver);
     }
 }
